b7d0dd25dd73216dbf5bf611f3cd062e492d0da3
Vandinho <evandro.menshaid@gmail.com>
2026-06-02 10:27:40 -0300

    MY10 - Refatorando o HEADER e FOOTER reduzindo os Includes

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ router.get('/', function(req, res, next) {
   menus.getMenus().then(results => {
     res.render('index', { 
       title: 'Restaurante Saboroso!',
-      menus: results
+      menus: results,
+      isHome: true
     });
   });
 });
