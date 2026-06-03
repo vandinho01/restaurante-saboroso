8bd8b6a0c685930e4098e573bfd469fe3632ddb0
Vandinho <evandro.menshaid@gmail.com>
2026-06-03 14:44:53 -0300

    MY22 - Administração do Menu - Consultando dados no MySQL

--- inc/menus.js
+++ inc/menus.js
@@ -5,8 +5,8 @@ module.exports = {
     return new Promise((resolve, reject) => {
       conn.query(
         `
-    SELECT * FROM tb_menus ORDER BY title
-    `,
+        SELECT * FROM tb_menus ORDER BY title
+        `,
         (err, results) => {
           if (err) {
             reject(err);
