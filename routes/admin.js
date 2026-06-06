0999e9c3b4389dde779701584549d5b9a2ad53ff
Vandinho <evandro.menshaid@gmail.com>
2026-06-06 11:18:21 -0300

    MY31 - Administração da reserva READ

--- routes/admin.js
+++ routes/admin.js
@@ -121,9 +121,12 @@
 
     router.get('/reservations', function(req, res, next){
 
-        res.render("admin/reservations", admin.getParams(req, {
-            date: {}
-        }));
+        reservations.getReservations().then(data => {
+            res.render("admin/reservations", admin.getParams(req, {
+                date: {},
+                data
+            }));
+        });
 
     });
 
