db265240ab19ac39a4095fe5c0ad4675e4b85282
Vandinho <evandro.menshaid@gmail.com>
2026-06-07 18:51:21 -0300

    MY39 - Administração dos usuários criando rotas

--- routes/admin.js
+++ routes/admin.js
@@ -163,7 +163,40 @@
 
     router.get('/users', function(req, res, next){
 
-        res.render("admin/users", admin.getParams(req));
+        users.getusers().then(data => {
+
+            res.render("admin/users", admin.getParams(req, {
+                data
+            }));
+
+        });
+
+
+    });
+    router.post('/users', function(req, res, next){
+
+        users.save(req.fields).then(results => {
+
+            res.send(results);
+
+        }).catch(err => {
+
+            res.send(err);
+
+        });
+
+    });
+    router.delete('/users:id', function(req, res, next){
+
+        users.dele(req.params.id).then(results => {
+
+            res.send(results);
+
+        }).catch(err => {
+
+            res.send(err);
+
+        });
 
     });
 
