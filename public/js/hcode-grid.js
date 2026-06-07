68e94d98f57643fad16d9798c45aeeb41ad45390
Vandinho <evandro.menshaid@gmail.com>
2026-06-07 18:36:10 -0300

    MY38 - Utilizando componente HcodeGrid na tela de Menus

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -113,18 +113,8 @@ class HcodeGrid {
                 let data = this.getTrData(e);
 
                 for (let name in data) {
-                    let input = this.formUpdate.querySelector(`[name=${name}]`);
 
-                    switch (name) {
-
-                        case 'date':
-                            if (input) input.value = moment(data[name]).format('YYYY-MM-DD');
-                            break;
-
-                        default:
-                            if (input) input.value = data[name];
-
-                    }
+                    this.options.onUpdateLoad(this.formUpdate, name, data);
 
                 }
 
