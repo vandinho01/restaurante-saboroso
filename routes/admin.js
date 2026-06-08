25004dc598029e9434b90645fc7de363242c0d25
Vandinho <evandro.menshaid@gmail.com>
2026-06-08 20:06:40 -0300

    MY43 - Administração dos e-mails recebidos

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,8 @@
     var admin = require('./../inc/admin');
     var menus = require('./../inc/menus');
     var reservations = require('./../inc/reservations');
-    var contacts = require('./../inc/contacts')
+    var contacts = require('./../inc/contacts');
+    var emails = require('./../inc/emails');
     var moment = require('moment');
     var router = express.Router();
 
@@ -98,9 +99,25 @@
 
     router.get('/emails', function(req, res, next){
 
-        res.render("admin/emails", admin.getParams(req));
+        emails.getEmails().then(data=>{
+            
+            res.render("admin/emails", admin.getParams(req, {
+                data
+            }));
+
+        });
+
 
     });
+
+    router.delete('/emails/:id', function(req, res, next) {
+
+        emails.delete(req.params.id).then(results => {
+            res.send(results);
+        }).catch(err => {
+            res.send(err);
+        })
+    })
 
     router.get('/menus', function(req, res, next){
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var conn = require('./../inc/db')
 var express = require('express');
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
-var reservations = require('./../inc/contacts');
 const contacts = require('./../inc/contacts');
+var emails = require('./../inc/emails');
 var router = express.Router();
 
 /* GET home page. */
@@ -98,6 +98,16 @@ router.get('/services', function(req, res, next){
     background: 'images/img_bg_1.jpg',
     h1: 'É um prazer poder servir!'
   });
+});
+
+router.post('/subscribe', function(req, res, next){
+
+  emails.save(req).then(results => {
+    res.json(results);
+  }).catch(err => {
+    res.json(err);
+  });
+
 });
 
 module.exports = router;
